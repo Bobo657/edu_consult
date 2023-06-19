9df5c5e3c35cdf0f76c59733ff579db28051a4c4
Bobo657 <47561918+Bobo657@users.noreply.github.com>
2023-06-19 10:48:51 +0100

    correction made

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,7 +13,7 @@
     </div>
 </div>
 <div class="map-sec space">  
-    <div class="map">
+    <div class="map" id="contact">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7310056272386!2d89.2286059153658!3d24.00527418490799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe9b97badc6151%3A0x30b048c9fb2129bc!2sAngfuzsoft!5e0!3m2!1sen!2sbd!4v1651028958211!5m2!1sen!2sbd"
             allowfullscreen=""
@@ -30,7 +30,7 @@
                 <div class="contact-info_icon"><i class="fal fa-location-dot"></i></div>
                 <div class="media-body">
                     <h4 class="contact-info_title">Our Address</h4>
-                    <span class="contact-info_text">{{ config('app.address')}}</span>
+                    <a href="#contact"><span class="contact-info_text">{{ config('app.address')}}</span></a>
                 </div>
             </div>
             <div class="contact-info">
